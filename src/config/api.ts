a115a809dcadb2934410d20e74773ba63727efe4
slagos <slagos@defontana.com>
2026-02-20 14:34:22 -0300

    url fix

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,7 @@
-const API_BASE = 'https://6gbja909qf.execute-api.us-east-1.amazonaws.com/prod';
+const LEADS_BASE = 'https://6gbja909qf.execute-api.us-east-1.amazonaws.com/prod';
+const VISIT_BASE = 'https://nzz30ikxtk.execute-api.us-east-1.amazonaws.com/prod';
 
 export const API = {
-  leads: `${API_BASE}/leads`,
-  visit: `${API_BASE}/visit`,
+  leads: `${LEADS_BASE}/leads`,
+  visit: `${VISIT_BASE}/visit`,
 };
